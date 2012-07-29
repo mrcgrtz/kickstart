18199a326e98df13fee56604f61942f8abf20e45
Marc Görtz <mail@marcgoertz.de>
2012-07-29 23:39:03 +0200

    Sample index file and robots.txt.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+$settings = array(
+	'classes'     => array('home'),
+	'title'       => 'Titel',
+	'description' => 'Beschreibung',
+);
+include('inc/header.inc.php');
+?>
+
+<h1>Startseite</h1>
+<p>Inhalt</p>
+
+<?php include('inc/footer.inc.php'); ?>
