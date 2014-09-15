6e32f3cd4d802c3513f15f97ed5f1c867cf03cde
Marc Görtz <mail@marcgoertz.de>
2014-09-15 15:11:51 +0200

    Fixed browser-update class name.

--- htdocs/inc/header.inc.php
+++ htdocs/inc/header.inc.php
@@ -59,7 +59,7 @@
 	</head>
 	<body class="<?php print implode(' ', $settings['classes']) ?>">
 
-		<!--[if lt IE 8]><div class="message message-update">Ihr Browser ist leider zu alt, um unsere Seite korrekt anzuzeigen. <a href="//windows.microsoft.com/de-de/internet-explorer/download-ie">Laden Sie hier die neueste Version herunter</a> oder <a href="//getfirefox.com">installieren Sie Mozilla Firefox</a>.</div><![endif]-->
+		<!--[if lt IE 8]><div class="browser-update">Ihr Browser ist leider zu alt, um unsere Seite korrekt anzuzeigen. <a href="//windows.microsoft.com/de-de/internet-explorer/download-ie">Laden Sie hier die neueste Version herunter</a> oder <a href="//getfirefox.com">installieren Sie Mozilla Firefox</a>.</div><![endif]-->
 
 		<!-- header -->
 		<header>
