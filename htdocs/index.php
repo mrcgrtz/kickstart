447bc1943a6b1ef13dd9ffc430ac06a7e45d57ed
Marc Görtz <mail@marcgoertz.de>
2017-11-01 15:47:52 +0100

    Remove H5/H6 styles, don't use them regularly.

--- htdocs/index.php
+++ htdocs/index.php
@@ -11,8 +11,6 @@ include_once(dirname(__FILE__) . '/inc/header.inc.php');
 <h2>h2. Überschrift</h2>
 <h3>h3. Überschrift</h3>
 <h4>h4. Überschrift</h4>
-<h5>h5. Überschrift</h5>
-<h6>h6. Überschrift</h6>
 
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 
