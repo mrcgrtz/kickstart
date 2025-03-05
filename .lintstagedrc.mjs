8cbc6765543924511433093f9e6811b47cdb5afe
Marc Görtz <mail@marcgoertz.de>
2025-03-05 23:52:33 +0100

    Update configuration file.

--- .lintstagedrc.mjs
+++ .lintstagedrc.mjs
@@ -1,6 +1,11 @@
-/** @type {Record<string, string | string[] | Promise<string | string[]>} */
-export default {
+/**
+ * @see https://github.com/lint-staged/lint-staged#configuration
+ * @type {import('lint-staged').Configuration}
+ */
+const config = {
 	'*.?({c,m}){j,t}s?(x)': ['prettier --write', 'xo --fix'],
 	'*.css': ['prettier --write', 'stylelint --fix'],
 	'*.(html|json|md?(x)|y?(a)ml)': 'prettier --write',
 };
+
+export default config;
