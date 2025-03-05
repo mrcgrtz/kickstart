c70cb0e7c912ccdafa75bb386846d966b3780b1a
Marc Görtz <mail@marcgoertz.de>
2025-03-05 23:51:20 +0100

    Migrate Prettier configuration to ESM.

--- .prettierrc.cjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('prettier').Config} */
-module.exports = {
-	trailingComma: 'all',
-	singleQuote: true,
-	bracketSpacing: false,
-};
---
+++ .prettierrc.mjs
@@ -0,0 +1,11 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import('prettier').Config}
+ */
+const config = {
+	trailingComma: 'all',
+	singleQuote: true,
+	bracketSpacing: false,
+};
+
+export default config;
